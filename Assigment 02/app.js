9e242c6a8b70edbac71f6e6a398c4eb210685006
qureshisameen 19 <qureshisameen@gmail.com>
2026-03-08 14:18:43 +0500

    'chapter2done'

---
+++ Assigment 02/app.js
@@ -0,0 +1,54 @@
+// Question no .1
+// var  studentName  = "Sameen"
+//    alert(studentName);
+//    document.write(studentName)
+//    console.log(studentName)
+
+
+// Question no .2
+// var  studentName  = " Sameen"
+//    alert(studentName);
+//    document.write(studentName)
+//    console.log(studentName)
+
+// Question no .3
+// a
+    // var message;         
+    
+    // b
+    // message = "Hello World";   
+
+    // c
+    // alert(message); 
+    
+    // Question no .4
+    // alert("Jhone Done")
+    // alert("21  years old")
+    // alert("Certified Mobile Application Development")
+
+      // Question no .5
+
+    // var text = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+    // alert(text);
+
+
+
+    // qUESTION No. 6
+
+// var email = "qureshisameen19@example.com";
+
+// alert("My email address is " + email);
+
+//  Question No.7
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+
+// Question No.8
+//  document.write("Yah! I can write HTML content through JavaScript");
+
+
+
+// Question No.9
+// let ass = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” "
+// alert(ass);
