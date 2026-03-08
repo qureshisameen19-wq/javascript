7da55f86f95dcb1f9970d879585f886b8b23afd6
qureshisameen 19 <qureshisameen@gmail.com>
2026-03-08 14:03:14 +0500

    completed

---
+++ Assigment 05/app.js
@@ -0,0 +1,209 @@
+
+// Q No .1
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1 + num2;
+
+// document.write("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+// Q No .2
+// let num1 = 3;
+// let num2 = 5;
+
+// Subtraction
+// let sub = num1 - num2;
+// document.getElementById("sub").innerHTML = "Subtraction of 3 and 5 is " + sub;
+
+// Multiplication
+// let mul = num1 * num2;
+// document.getElementById("mul").innerHTML = "Multiplication of 3 and 5 is " + mul;
+
+// Division
+// let div = num1 / num2;
+// document.getElementById("div").innerHTML = "Division of 3 and 5 is " + div;
+
+// Modulus
+// let mod = num1 % num2;
+// document.getElementById("mod").innerHTML = "Modulus of 3 and 5 is " + mod;
+
+// Q No .3
+
+        // a. .
+        // let myVariable;
+
+        // b. 
+        // document.write("Value after variable declaration is: " + myVariable + "<br>");
+
+        // c. 
+        // myVariable = 5;
+
+        // d. 
+        // document.write("Initial value: " + myVariable + "<br>");
+
+        // e.
+        // myVariable++;
+
+        // f. 
+        // document.write("Value after increment is: " + myVariable + "<br>");
+
+        // g.
+        // myVariable = myVariable + 7;
+
+        // h. 
+//         document.write("Value after addition is: " + myVariable + "<br>");
+
+// let num1 = 10;
+// let num2 = 5;
+
+// --- Subtraction ---
+// let difference = num1 - num2;
+
+
+// --- Multiplication ---
+// let product = num1 * num2;
+// console.log(`The product of ${num1} and ${num2} is: ${product}`);
+
+
+// --- Division ---
+// let quotient = num1 / num2;
+// console.log(`The quotient of ${num1} and ${num2} is: ${quotient}`);
+
+
+
+// Q No .4
+// var ticketPrice = 600;
+
+// var numberOfTickets = 5;
+// var totalCost = ticketPrice * numberOfTickets;
+
+// document.write("Total cost to buy " + numberOfTickets + " tickets to a movie is " + totalCost + "PKR");
+
+// Q No .5
+// let count  = 1
+// let tableNum = 4
+
+// document.write("Table of 4")
+// document.write("<br />")
+
+// document.write(`${tableNum} x ${count} = ${tableNum}`)
+// document.write("<br />")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum *count}`)
+// document.write("<br />")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum *count}`)
+// document.write("<br />")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum *count}`)
+// document.write("<br />")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum *count}`)
+// document.write("<br />")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum *count}`)
+// document.write("<br />")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum *count}`)
+// document.write("<br />")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum *count}`)
+// document.write("<br />")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum *count}`)
+// document.write("<br />")
+// count++
+// document.write(`${tableNum} x ${count} = ${tableNum *count}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
