7e957312be94bd7a06c5b75faa3ae5c04d827f0f
Qureshisameen <qureshisameen19@gmail.com>
2026-04-04 15:06:01 +0500

    'done'

---
+++ Assigment 06-09/app.js
@@ -0,0 +1,167 @@
+// Q Node.1
+    // var a = 10;
+
+    // document.write("Result:<br>");
+    // document.write("The value of a is: " + a + "<br>");
+    // document.write("-----------------------------<br><br>");
+
+    // Pre-increment
+    // document.write("The value of ++a is: " + (++a) + "<br>");
+    // document.write("Now the value of a is: " + a + "<br><br>");
+ // Post-increment
+    // document.write("The value of a++ is: " + (a++) + "<br>");
+    // document.write("Now the value of a is: " + a + "<br><br>");
+
+
+    // // Pre-decrement
+    // document.write("The value of --a is: " + (--a) + "<br>");
+    // document.write("Now the value of a is: " + a + "<br><br>");
+
+    // Post-decrement
+    // document.write("The value of a-- is: " + (a--) + "<br>");
+    // document.write("Now the value of a is: " + a + "<br>");
+
+// Q #2
+        // Initializing variables
+        // var a = 2, b = 1;
+
+        // The complex expression execution
+        // Stage 1: --a (a becomes 1)
+        // Stage 2: --b (b becomes 0)
+        // Stage 3: ++b (b becomes 1)
+        // Stage 4: b-- (uses 1, then b becomes 0)
+        // var result = --a - --b + ++b + b--;
+
+        // Displaying results on the page
+        // document.write("a is " + a + "<br>");
+        // document.write("b is " + b + "<br>");
+        // document.write("result is " + result);
+
+        // Q No. 3
+        
+// let userName = prompt("User Name")
+
+// if(userName = "name"){
+// alert("Welcome to the Page")
+// }
+
+// Q #4
+
+// let num = +prompt("any number");
+
+// if (num >= 0) {
+//   let multiple = 1;
+// //   document.write(`Table of ${num}`);
+// //   document.write("<br />");
+
+// //   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${num} x ${multiple} = ${num * multiple}`);
+// } else {
+//   document.write(`Table of 5`);
+//   document.write("<br />");
+//   let table = 5;
+//   let multiple = 1;
+
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+//   document.write("<br />");
+
+//   multiple++;
+//   document.write(`${table} x ${multiple} = ${table * multiple}`);
+// }
+
+// Q #5
+
+// let subject1 = prompt("1st Subject")
+// let subject1Marks = +prompt("1st Subject Marks")
+
+// let subject2 = prompt("2nd Subject")
+// let subject2Marks = +prompt("2nd Subject Marks")
+
+// let subject3 = prompt("3rd Subject")
+// let subject3Marks = +prompt("3rd Subject Marks")
+
+// let totalMarks = 100
+
+// document.write(`Subject Total Marks Obtained Mraks Percentage`)
+// document.write("<br />")
+// document.write(`${subject1} ${totalMarks} ${subject1Marks} ${subject1Marks}%`)
+
+// document.write("<br />")
+// document.write(`${subject2} ${totalMarks} ${subject2Marks} ${subject2Marks}%`)
+
+// document.write("<br />")
+// document.write(`${subject3} ${totalMarks} ${subject3Marks} ${subject3Marks}%`)
+
+// let marks = totalMarks * 3
+// let obtainMarks = subject1Marks + subject2Marks + subject3Marks
+// let percentage = obtainMarks / marks * 100
+
+// document.write("<br />")
+// document.write(`${marks} ${obtainMarks} ${percentage}`)
